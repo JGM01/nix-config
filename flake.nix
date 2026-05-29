6dc1bccca9c0b4012b34aa28216593ca5d4ccda0
JGM01 <jacobgermanamccray@gmail.com>
2026-05-29 19:27:17 -0400

    server

--- flake.nix
+++ flake.nix
@@ -5,17 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
+		nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 
-  outputs = { self, nixpkgs, agenix }:
+  outputs = { self, nixpkgs, agenix, nix-minecraft }@inputs:
     {
       nixosConfigurations.trollserver =
         nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
 
+					specialArgs = { inherit inputs; };
           modules = [
             ./configuration.nix
-	    agenix.nixosModules.default
+	    			agenix.nixosModules.default
           ];
         };
     };
